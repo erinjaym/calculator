55a8a8bb474b065b47dcc8e8619f6ec0b163a538
EJM <erin.jay.moyer@gmail.com>
2021-03-14 14:36:35 -0600

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,32 @@
+
+function operate (operator, number1, number2) 
+{
+
+number1 = 0;
+}
+
+
+function calculate (operate) 
+{
+
+    function add ()
+    {
+
+    }
+
+    function subtract ()
+    {
+
+    }
+
+    function multiply ()
+    {
+
+    }
+
+    function divide ()
+    {
+
+    } 
+
+}
