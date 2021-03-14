183db023b99e2c9fa18ee26824273d0e0191d8af
EJM <erin.jay.moyer@gmail.com>
2021-03-14 15:25:50 -0600

    Creating base with basic functions and a total

--- calculator.js
+++ calculator.js
@@ -1,31 +1,71 @@
 
+let total = 0; 
+let lastTotal = 0; 
+
+
+// operator assignement with clicks of a button or ASCII assignment
+
 function operate (operator, number1, number2) 
 {
+    if (operator == "addition")
+    {
+        lastTotal = total;
+        total += add(number1, number2);
+        return total;
+    }
+    else if (operator == "subtraction")
+    {
+        lastTotal = total;
+        total += subtract(number1, number2);
+        return total;
 
+    }
+    else if (operator == "multiplication")
+    {
+        lastTotal = total;
+        total += multiply(number1, number2);
+        return total;
+        
+    }
+    else if (operator == "division")
+    {
+        lastTotal = total;
+        total += divide(number1, number2);
+        return total;
+        
+    }
+    else // base case or something went wrong
+    {
+    return 80085;
+    }
 }
 
 
-function calculate (operate) 
-{
 
-    function add ()
+ 
+// operation functions 
+    function add (number1, number2) 
     {
-
+    return number1 + number2;
+    }
+        
+    function subtract (number1, number2) 
+    {
+    return number1 - number2; 
     }
 
-    function subtract ()
+    function multiply (number1, number2) 
     {
-
+    return number1 * number2;
     }
 
-    function multiply ()
+    function divide (number1, number2)
     {
-
-    }
-
-    function divide ()
-    {
-
+    return number1 / number2;
     } 
 
-}
+    function clear()
+    {
+    return total = 0;
+    }
+
